3c61f155593f2ff6b5211e1e28ce3ef3fbf55274
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-14 16:33:55 +0200

    add innerhtml to cell fizz

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ for (let i = 1; i <= 100; i = i + 1) {
         elegrid.innerHTML = elegrid.innerHTML + `<div class="cell-fizz-buzz">${i}</div>`;
     } else if (i % 3 == 0) {
         console.log('Fizz');
-
+        elegrid.innerHTML = elegrid.innerHTML + `<div class="cell-fizz">${i}</div>`;
     } else if (i % 5 == 0) {
         console.log('Buzz');
     } else {
