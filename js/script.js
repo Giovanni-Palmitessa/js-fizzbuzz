2a51dd3ab256cbffb566a581574fe08619264d14
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-14 16:33:24 +0200

    add innerhtml to fizz buzz

--- js/script.js
+++ js/script.js
@@ -34,8 +34,10 @@ for (let i = 1; i <= 100; i = i + 1) {
 
     if (i % 3 == 0 && i % 5 == 0 ) {
         console.log('FizzBuzz');
+        elegrid.innerHTML = elegrid.innerHTML + `<div class="cell-fizz-buzz">${i}</div>`;
     } else if (i % 3 == 0) {
         console.log('Fizz');
+
     } else if (i % 5 == 0) {
         console.log('Buzz');
     } else {
