00912e927e322999c9ad06f33eafcbceee1befe1
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-14 15:34:26 +0200

    add example

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ ESEMPIO
 1) i = 0;  in console si ha 0
 2) i = 1;  in console si ha 1
 3) i = 2;  in console si ha 2
+4) i = 3;  in console si ha Fizz
+5) i = 4;  in console si ha 4
+6) i = 5;  in console si ha Buzz
 
 
 
